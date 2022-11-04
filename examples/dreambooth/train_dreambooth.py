cced78196ab2440b652c860e6186aeda73cda024
Ben <110583491+TheLastBen@users.noreply.github.com>
2022-11-04 14:25:27 +0100

    Update train_dreambooth.py

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -732,7 +732,7 @@ def main():
                      if args.train_text_encoder and os.path.exists(frz_dir):
                         subprocess.call('rm -r '+save_dir+'/text_encoder/*.*', shell=True)
                         subprocess.call('cp -f '+frz_dir +'/*.* '+ save_dir+'/text_encoder', shell=True)                     
-                     chkpth=args.Session_dir+inst+".ckpt"
+                     chkpth=args.Session_dir+"/"+inst+".ckpt"
                      subprocess.call('python /content/diffusers/scripts/convert_diffusers_to_original_stable_diffusion.py --model_path ' + save_dir + ' --checkpoint_path ' + chkpth + ' --half', shell=True)
                      i=i+args.save_n_steps
             
